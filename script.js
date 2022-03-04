1992b7dfaf4ac7adda8cce80cf40d0d463e368b4
mahdiedostmohamadi <92886168+mahdiedostmohamadi@users.noreply.github.com>
2022-03-04 21:56:42 +0330

    10

--- script.js
+++ script.js
@@ -9,13 +9,42 @@ const data = async () => {
 };
 data();
 const series = (movies) => {
-    main=document.querySelector('main')
+    main = document.querySelector("main");
     for (let movie of movies) {
-card=document.createElement('div')
-main.appendChild(card)
-const a=document.createElement('a')
-a.href=movie.url
-a.textContent=movie.name
+        card = document.createElement("div");
+        card.classList.add("card");
+        main.appendChild(card);
+        const a = document.createElement("a");
+        a.classList.add("a");
 
+        a.href = movie.url;
+        a.target = "_blank";
+        a.textContent = movie.name;
+        const img = document.createElement("img");
+        img.src = movie.image.medium;
+        img.classList.add("img");
+        const rate = document.createElement("span");
+        const airtime = document.createElement("span");
+        rate.classList.add("AR");
+        airtime.classList.add("AR");
+
+        rate.textContent = `rate:${movie.rating.average}`;
+        airtime.textContent = `airtime:${movie.airtime}`;
+        const summary=document.createElement('p')
+        summary.innerHTML=movie.summary
+        summary.style.display="none"
+
+        card.addEventListener("mouseleave", () => {
+            summary.style.display="none"
+
+        });
+        
+        card.addEventListener("mouseover", () => {
+            summary.style.display=""
+
+        });
+        
+
+        card.append(a, img,airtime,rate,summary);
     }
 };
